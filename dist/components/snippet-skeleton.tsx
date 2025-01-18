151687747b4ef8b95141ccf2c241b450c0146aaa
Adrian <adrianmanea.s@icloud.com>
2025-01-18 19:26:49 +0200

    Wip

---
+++ dist/components/snippet-skeleton.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Text, Skeleton, Flex } from '@radix-ui/themes';
+
+export const SnippetLoadingSkeleton = () => {
+  return (
+    <Flex direction="column" gap="4">
+      <Flex direction="column" gap="2">
+        <Text>
+          <Skeleton>Lorem ipsum dolor sit amet.</Skeleton>
+        </Text>
+
+        <Skeleton>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</Text>
+        </Skeleton>
+      </Flex>
+    </Flex>
+  );
+};
